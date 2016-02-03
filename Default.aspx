004d62160cecb515eb6927797c377791510e7bce
pianohagens <pianohagens@gmail.com>
2016-02-03 14:03:40 -0800

    commitSelectWebClient

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SelectWebClient</title>
+</head>
+<body>
+    <form id="form1" runat="server" >
+    <div>
+        <asp:DropDownList ID="DropDownList1" runat="server" ></asp:DropDownList>
+      
+        <asp:GridView ID="GridView1" runat="server" ></asp:GridView>
+       <asp:Button ID="Button1" runat="server" Text="Get Shows" OnClick="Button1_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    SelectWebClientServiceReference.ServiceClient sc = new SelectWebClientServiceReference.ServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string[] venues = sc.GetVenues();
+            DropDownList1.DataSource = venues;
+            DropDownList1.DataBind();
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        string venue = DropDownList1.SelectedItem.Text;
+
+        SelectWebClientServiceReference.ShowInfo[] shows = sc.GetShowsByVenue(venue);
+       
+        GridView1.DataSource = shows;
+        GridView1.DataBind();
+
+    }
+}
